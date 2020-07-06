5f467d7b61345fc21ee98bed2525022b67df55b4
AlanWen2016 <906075319@qq.com>
2020-07-06 13:59:42 +0800

    jsx learning

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { red } from 'color-name'
+import { ReactComponent } from '*.svg'
+import propTypes from 'prop-types'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+let root: HTMLElement | null = document.getElementById('root')
+// let ele  = React.createElement('h1', null, 'hello')
+let title:string = 'Hello'
+// console.log(ele)
+let titleStyle: React.CSSProperties = { color: 'red', fontSize: 24}
+
+function tick(){
+    const ele: React.ReactElement = (
+        <div>{new Date().toLocaleTimeString()}</div>
+    )
+    ReactDOM.render(ele, root)
+}
+
+
+// setInterval(tick, 1000)
+
+interface Props {
+    name: string
+}
+function Welcome(props: Props):React.ReactElement {
+    return <h1>hello {props.name}</h1>
+
+}
+
+// ReactDOM.render(<Welcome name="abc"/>, root)
+
+
+
+class Welcome2 extends React.Component<Props>{
+    render(): React.ReactElement{
+        return <h1>hello {this.props.name}</h1>
+    }
+}
+// ReactDOM.render(<Welcome2 name="efg"/>, root)
+
+
+interface PersonProps{  
+    name?: string,
+    gender?: 'male' | 'female'
+
+}
+class Person extends React.Component<PersonProps>{
+    static defaultProps: PersonProps = {
+        name: 'unname'
+    }
+
+    static propTypes ={
+        name: propTypes.string.isRequired,
+        gender: propTypes.oneOf(['male', 'female']).isRequired
+    }
+    render() {
+        return <div>{this.props.name}</div>
+    }
+
+}
+
+let personProps: PersonProps = {
+    name: 'test'
+}
+
+
+ReactDOM.render(<Person {...personProps} />, root)
+
+// ReactDOM.render(<h2 className="title" style={titleStyle}>{title}</h2>, root)
+
